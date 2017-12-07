ee51ee1c3d0829f41c2c2a14644d8a1e6ff1ff54
艾尔尼亚-A <1173197065@qq.com>
2017-12-07 13:06:50 +0800

    2

--- test.php
+++ test.php
@@ -1,2 +1,5 @@
 <?php
-var_dump($_GET);
+$code=$_GET['code'];
+$nsukey=$_GET['nsukey'];
+
+echo "<script>window.location.href='http://127.0.0.1:12138/wShop/index.php/home/test/test/code/$code/nsukey/$nsukey'</script>";
