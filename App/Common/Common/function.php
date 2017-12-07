2b6db9f6f98e5675aad60f22af43d8270d4d6775
艾尔尼亚-A <1173197065@qq.com>
2017-12-07 15:55:25 +0800

    appid

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -64,7 +64,13 @@ function _request($curl,$https=true,$method='GET',$data=null){
 function baseAuth($redirect_url){
     $appid='wx9b7ab18e61268efb';
     $appsecret='bcd46807674b9448617438256db6cada';
+    //===
+    $appid='wxc5919bd34da8b695';
+    $appsecret='87e678bca54b92f8c7213e1ba9f12963';
+    
+    
     //1.准备scope为 snsapi_base 网页授权页面 snsapi_userinfo
+    
     $baseurl = urlencode($redirect_url);
     $snsapi_base_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri='.$baseurl.'&response_type=code&scope=snsapi_base&state=YQJ#wechat_redirect';
     
